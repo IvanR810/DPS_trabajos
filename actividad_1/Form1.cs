4a72f5c5f42b959484f426173a8befc69d78f79c
IvanR3 <ivan810rivera@gmail.com>
2023-08-28 21:17:52 -0500

    feat: agregando trabajo TAI1

---
+++ actividad_1/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Sistema_Ivan
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(textBox1.Text == "admin"){
+                label4.Text = "👍 Bienvenido " + textBox1.Text;
+            }
+            else{
+                label4.Text = "Los datos no son correctos";
+            }
+        }
+    }
+}
